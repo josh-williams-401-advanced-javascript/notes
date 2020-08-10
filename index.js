880555acca2a7932dbdc64a8bb917447dfe01d74
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-08-10 16:43:34 -0700

    Notes and input woring

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+'use strict';
+
+const Input = require('./lib/input');
+const Notes = require('./lib/notes');
+const { Console } = require('console');
+
+const newNote = new Input;
+new Notes(newNote).execute()
